a6b69252a74f294e323ca763bd0bc7857fc5ff76
Felipe Pinheiro <f.pinheir0@outlook.com>
2023-07-21 22:42:56 -0300

    update main

--- diagram/lib/main.dart
+++ diagram/lib/main.dart
@@ -1,5 +1,15 @@
+import 'package:diagram/pages/first_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp;
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: firstPage(),
+    );
+  }
 }
---
+++ diagram/lib/pages/diagram.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DiagramPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Diagram test'),
+      ),
+      body: Center(
+        child: Text('Test on'),
+      ),
+    );
+  }
+}
