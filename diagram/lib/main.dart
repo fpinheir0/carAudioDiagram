44d4aef02a165ae116bd9fba51c7182ce8edfaf1
Felipe Pinheiro <f.pinheir0@outlook.com>
2023-07-21 22:43:09 -0300

    Revert "update main"
    This reverts commit a6b69252a74f294e323ca763bd0bc7857fc5ff76.

--- diagram/lib/main.dart
+++ diagram/lib/main.dart
@@ -1,15 +1,5 @@
-import 'package:diagram/pages/first_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: firstPage(),
-    );
-  }
+  runApp;
 }
